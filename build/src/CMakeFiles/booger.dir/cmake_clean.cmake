275796048053d4e1684914279ee589f16cbfc900
grant <grant.lathrom@gmail.com>
2022-02-24 20:27:33 -0600

    install in home directory working

---
+++ build/src/CMakeFiles/booger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/booger.dir/booger.cpp.o"
+  "libbooger.pdb"
+  "libbooger.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/booger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
